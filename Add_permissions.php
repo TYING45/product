6b125ccfaacc52e605dbfaa1a72f01e4ed46fbab
TYING45 <2278050375a@gmail.com>
2025-06-08 12:47:18 +0800

    Update Add_permissions.php

--- Add_permissions.php
+++ Add_permissions.php
@@ -24,20 +24,20 @@ if (isset($_POST["action"]) && $_POST["action"] == "add") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="CSS/Add_Del.css" rel="stylesheet" type="text/css">
-    <title>新增權限資料</title>
+    <title>新增管理員</title>
 </head>
 <body>
 <head>
 <link href="CSS/form.css" rel="stylesheet" type="text/css">
 <main>
-<p><b><font size="5">新增管理員系統</font></b></p>
+<h1>新增管理員系統</h1>
 <form method="POST" action="">
  <table id = "Permission" >
     <tr>
         <th>欄位</th><th>資料</th></tr>
 		<tr><td>管理員編號</td><td><input type="text" name ="UserID"id="UserID" required></td></tr>
 		<tr><td>姓名</td><td><input type="text" name ="name"id="name" required></td></tr>
-        <tr><td>帳號</td><td><input type="text" name ="username"id="username" required></td></tr>
+        	<tr><td>帳號</td><td><input type="text" name ="username"id="username" required></td></tr>
 		<tr><td>密碼</td><td><input type="text" name ="password"id="password" required></td></tr>
 		<tr><td>電話</td><td><input type="text" name ="Phone"id="Phone" required></td></tr>
 		<tr><td>email</td><td><input type="text" name ="Email"id="Email" required></td></tr>
