34968855be1ef4968ab8e271fda37503bec274a1
TYING45 <2278050375a@gmail.com>
2025-06-09 13:04:42 +0800

    Update Seller_Order.php

--- Seller_Order.php
+++ Seller_Order.php
@@ -183,8 +183,8 @@ $result = $stmt->get_result();
         </select>
         <select name="payment_status">
             <option value="">全部付款狀態</option>
-            <option value="尚未付款" <?= $payment_status === '尚未付款' ? 'selected' : '' ?>>尚未付款</option>
-            <option value="已付款" <?= $payment_status === '已付款' ? 'selected' : '' ?>>已付款</option>
+            <option value="尚未繳款" <?= $payment_status === '尚未繳款' ? 'selected' : '' ?>>尚未繳款</option>
+            <option value="已繳款" <?= $payment_status === '已繳款' ? 'selected' : '' ?>>已繳款</option>
         </select>
         <select name="order_status">
             <option value="">全部訂單狀態</option>
