da4de7f753c1b11aeeecbcc80009ce55cbce14f6
TYING45 <2278050375a@gmail.com>
2025-06-09 23:37:56 +0800

    Update Update_Order.php

--- Update_Order.php
+++ Update_Order.php
@@ -228,7 +228,20 @@ $total_amount = $order['total_price'] + $shipping_fee;
                             ?>
                         </select>
                     </td>
-                  <td class="content"><?= htmlspecialchars($order['Payment_status'] ?? '') ?></td>
+                  <td>
+                <?php
+                $payment_method = $order['Payment_method'] ?? '';
+                $order_status = $order['Order_status'] ?? '';
+
+                if ($payment_method === 'cc') {
+                echo '已繳款';
+                } elseif ($payment_method === 'cod' && $order_status === '結案') {
+                echo '已繳款';
+                } else {
+                echo '尚未繳款';
+                }
+                ?>
+                </td>
                     <td class="content">多項</td>
                     <td class="content"><input type="date" name="Ship_Date" value="<?= htmlspecialchars($order['Ship_Date'] ?? '') ?>"></td>
                     <td class="content"><input type="text" name="Transport" value="<?= htmlspecialchars($order['Transport'] ?? '') ?>"></td>
