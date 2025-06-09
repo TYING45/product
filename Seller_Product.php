30a5da3841110404f726aa0655a15637cc33a9d7
TYING45 <2278050375a@gmail.com>
2025-06-09 14:41:22 +0800

    Update Seller_Product.php

--- Seller_Product.php
+++ Seller_Product.php
@@ -92,7 +92,7 @@ function keepURL()
     <ul class="topmenu">
         <li><button onclick="toggleSidebar()" class="img-button"></button></li>
         <li></li>
-        <li><a href="#">網頁前端</a></li>
+        <li><a href="https://secondhandshop.netlify.app/">網頁前端</a></li>
         <li><a href="logout.php">登出</a></li>
     </ul>   
 </div>
@@ -100,10 +100,6 @@ function keepURL()
 <div id="leftside">
     <ul class="menuleft">
         <li><a href="Seller_index.php">首頁</a></li>
-        <li>
-            <a href="#" onclick="toggleMenu(event)">網站管理系統</a>
-            <ul class="menuleft_hide"><li><a href="#">網站管理</a></li></ul>
-        </li>
         <li>
             <a href="#" onclick="toggleMenu(event)">商品管理系統</a>
             <ul class="menuleft_hide">
